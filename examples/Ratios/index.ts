62d2d33a58fa7807d6b13b80c377d74f09f27341
Bas <bas-can@hotmail.com>
2021-11-13 13:21:37 +0100

    Add 'ratios' example

---
+++ examples/Ratios/index.ts
@@ -0,0 +1,87 @@
+/// <reference path="../../lib/openrct2.d.ts" />
+
+import { box, horizontal, label, vertical, window } from "openrct2-fluentui";
+
+
+const ratios = window({
+	title: "Ratios (fui example)",
+	width: 400, minWidth: 100, maxWidth: 10000,
+	height: 300, minHeight: 75, maxHeight: 7500,
+	padding: 5,
+	content: [
+		horizontal([
+			box(
+				label({
+					text: "#1",
+					alignment: "centred",
+					padding: [ "50%", 0 ]
+				})
+			),
+			vertical([
+				box(
+					label({
+						text: "#2",
+						alignment: "centred",
+						padding: [ "50%", 0 ]
+					})
+				),
+				horizontal([
+					vertical([
+						horizontal([
+							box(
+								label({
+									text: "#5",
+									alignment: "centred",
+									padding: [ "50%", 0 ]
+								})
+							),
+							vertical([
+								box(
+									label({
+										text: "#6",
+										alignment: "centred",
+										padding: [ "50%", 0 ]
+									})
+								),
+								box(
+									label({
+										text: ":-)",
+										alignment: "centred",
+										padding: [ "50%", 0 ]
+									})
+								)
+							])
+						]),
+						box(
+							label({
+								text: "#4",
+								alignment: "centred",
+								padding: [ "50%", 0 ]
+							})
+						),
+					]),
+					box(
+						label({
+							text: "#3",
+							alignment: "centred",
+							padding: [ "50%", 0 ]
+						})
+					),
+				])
+			])
+		])
+	]
+});
+
+
+registerPlugin({
+	name: "Ratios (fui-example)",
+	version: "1.0",
+	authors: ["Basssiiie"],
+	type: "local",
+	licence: "MIT",
+	main: () =>
+	{
+		ui.registerMenuItem("(fui) Ratios", () => ratios.open());
+	}
+});
