d09f6fd9d7f345e966c8a74f11e7b1a980989e69
Bas <bas-can@hotmail.com>
2021-11-15 21:49:45 +0100

    Remove unused array functions

--- tests/utilities/array.tests.ts
+++ tests/utilities/array.tests.ts
@@ -4,54 +4,16 @@ import test from "ava";
 import * as ArrayHelper from "@src/utilities/array";
 
 
-test("isValidIndex()", t =>
+test("find()", t =>
 {
 	const array = [ "one", "two", "three" ];
 
-	t.true(ArrayHelper.isValidIndex(array, 0));
-	t.true(ArrayHelper.isValidIndex(array, 1));
-	t.true(ArrayHelper.isValidIndex(array, 2));
+	t.is(ArrayHelper.find(array, i => i.startsWith("o")), "one");
+	t.is(ArrayHelper.find(array, i => i.startsWith("tw")), "two");
+	t.is(ArrayHelper.find(array, i => i.startsWith("th")), "three");
+	t.is(ArrayHelper.find(array, () => true), "one");
 
-	t.false(ArrayHelper.isValidIndex(array, -1));
-	t.false(ArrayHelper.isValidIndex(array, 3));
-	t.false(ArrayHelper.isValidIndex(array, 5));
-	t.false(ArrayHelper.isValidIndex(array, null));
-	t.false(ArrayHelper.isValidIndex(array, undefined!));
-	t.false(ArrayHelper.isValidIndex([], 0));
-	t.false(ArrayHelper.isValidIndex(null, 0));
-	t.false(ArrayHelper.isValidIndex(undefined!, 0));
-});
-
-
-test("getAtIndex()", t =>
-{
-	const array = [ "one", "two", "three" ];
-
-	t.is(ArrayHelper.getAtIndex(array, 0), "one");
-	t.is(ArrayHelper.getAtIndex(array, 1), "two");
-	t.is(ArrayHelper.getAtIndex(array, 2), "three");
-
-	t.is(ArrayHelper.getAtIndex(array, -1), null);
-	t.is(ArrayHelper.getAtIndex(array, 3), null);
-	t.is(ArrayHelper.getAtIndex(array, 5), null);
-	t.is(ArrayHelper.getAtIndex(array, null), null);
-	t.is(ArrayHelper.getAtIndex(array, undefined!), null);
-	t.is(ArrayHelper.getAtIndex([], 0), null);
-	t.is(ArrayHelper.getAtIndex(null, 0), null);
-	t.is(ArrayHelper.getAtIndex(undefined!, 0), null);
-});
-
-
-test("findIndex()", t =>
-{
-	const array = [ "one", "two", "three" ];
-
-	t.is(ArrayHelper.findIndex(array, i => i.startsWith("o")), 0);
-	t.is(ArrayHelper.findIndex(array, i => i.startsWith("tw")), 1);
-	t.is(ArrayHelper.findIndex(array, i => i.startsWith("th")), 2);
-	t.is(ArrayHelper.findIndex(array, () => true), 0);
-
-	t.is(ArrayHelper.findIndex(array, i => i.startsWith("z")), null);
-	t.is(ArrayHelper.findIndex(array, () => false), null);
-	t.is(ArrayHelper.findIndex([], () => false), null);
+	t.is(ArrayHelper.find(array, i => i.startsWith("z")), null);
+	t.is(ArrayHelper.find(array, () => false), null);
+	t.is(ArrayHelper.find([], () => false), null);
 });
