071a0f6b72bb1afef10c13bccc8b970eaffd9229
Bas <Basssiiie@users.noreply.github.com>
2023-03-19 22:21:47 +0100

    Compute now returns writable stores

--- src/bindings/stores/compute.ts
+++ src/bindings/stores/compute.ts
@@ -1,6 +1,7 @@
 import { store } from "./createStore";
 import { Store } from "./store";
 import { subscribe } from "./subscribe";
+import { WritableStore } from "./writableStore";
 
 
 /**
@@ -8,11 +9,11 @@ import { subscribe } from "./subscribe";
  * one-way binding. If one of the parent stores dispatches an update, the new store
  * recomputes itself automatically and will notify its subscribers of the change.
  */
-export function compute<T, U>(store: Store<T>, callback: (value: T) => U): Store<U>;
-export function compute<T1, T2, U>(first: Store<T1>, second: Store<T2>, callback: (first: T1, second: T2) => U): Store<U>;
-export function compute<T1, T2, T3, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, callback: (first: T1, second: T2, third: T3) => U): Store<U>;
-export function compute<T1, T2, T3, T4, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, fourth: Store<T4>, callback: (first: T1, second: T2, third: T3, fourth: T4) => U): Store<U>;
-export function compute<T1, T2, T3, T4, T5, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, fourth: Store<T4>, fifth: Store<T5>, callback: (first: T1, second: T2, third: T3, fourth: T4, fifth: T5) => U): Store<U>;
+export function compute<T, U>(store: Store<T>, callback: (value: T) => U): WritableStore<U>;
+export function compute<T1, T2, U>(first: Store<T1>, second: Store<T2>, callback: (first: T1, second: T2) => U): WritableStore<U>;
+export function compute<T1, T2, T3, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, callback: (first: T1, second: T2, third: T3) => U): WritableStore<U>;
+export function compute<T1, T2, T3, T4, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, fourth: Store<T4>, callback: (first: T1, second: T2, third: T3, fourth: T4) => U): WritableStore<U>;
+export function compute<T1, T2, T3, T4, T5, U>(first: Store<T1>, second: Store<T2>, third: Store<T3>, fourth: Store<T4>, fifth: Store<T5>, callback: (first: T1, second: T2, third: T3, fourth: T4, fifth: T5) => U): WritableStore<U>;
 export function compute<U>(...args: [...Store<unknown>[], ((...values: unknown[]) => U)]): Store<U>
 {
 	const stores = <Store<unknown>[]>args.slice(0, -1);
