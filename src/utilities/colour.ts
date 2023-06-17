179821e8badef0413106524b87d53bedf33415c4
Arjan van Dijk <71702374+Manticore-007@users.noreply.github.com>
2023-06-17 22:03:00 +0200

    Update colour.ts
    removed the comment:
     * These are the regular selectable colours from left-to-right and top-to-bottom.

--- src/utilities/colour.ts
+++ src/utilities/colour.ts
@@ -1,6 +1,5 @@
 /**
  * All the available colours in OpenRCT2 for windows and colour pickers.
- * These are the regular selectable colours from left-to-right and top-to-bottom.
  */
 export const enum Colour
 {
